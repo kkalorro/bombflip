1f8f3bb7926e3063530ad0d382bb82a47db32a12
kkalorro <kkalorro@gmail.com>
2020-04-08 15:23:47 -0700

    Actual values are now hidden. Revealed values are styled. Game ends when Bomb (0 points) revealed. Restart Game function added.  Score is now implemented.  Bombs are now indicated at start.  Styled to fit mobile.

--- js/generate.js
+++ js/generate.js
@@ -4,8 +4,22 @@
 // Init //
 //////////
 
+// Game is Active
+let gameover = true;
+
+// Points
+let points = 0;
+
 // Select Left Playing Space
-const selectLeftPlayspace = document.getElementById('left-playspace');
+const selectPlayspace = document.getElementById('playspace');
+// Select Restart Button
+const selectRestart = document.getElementById('restart');
+// Select Score
+const selectScore = document.getElementById('score');
+// Select Bombs Guessed
+const selectBombsGuessed = document.getElementById('bombs-guessed');
+// Select Bombs Total
+const selectBombCount = document.getElementById('bomb-count');
 
 // Unique ID Counter
 let idCount = 0;
@@ -30,7 +44,14 @@ function randNum(max) {
     return Math.floor(Math.random() * max);
 }
 
+function resetScore() {
+    points = 0;
+    selectScore.textContent = points;
+}
+
 function populateAnswers() {
+    // Reset Answers just in case this we're restarting a game
+    answers.splice(0, answers.length)
     // Calculated size of whole grid and hint spaces
     const total = Math.pow(gridSize, 2) + (gridSize * 2);
     // Helper variable to help identify the last column and last row
@@ -38,7 +59,7 @@ function populateAnswers() {
     for (let i = 0; i < total; i++) {
         // Give Answer a Unique ID
         const answer = {id: i};
-        idCount += 1;
+        idCount++;
         // Identify last column or row to use as Hints
         if (hintCount === gridSize  || i + gridSize >= total) {
             answer.hint = true;
@@ -47,7 +68,7 @@ function populateAnswers() {
         } else {
             // Designate a number from 0-3 (0 = Bomb, 1-3 = Points)
             answer.value = randNum(4);
-            hintCount += 1;
+            hintCount++;
         }
         answers.push(answer);
     }
@@ -59,11 +80,23 @@ function getValue(array, searchFor) {
         // If i matches searchFor
         if (array[i].id == searchFor) {
         // return Value
-            console.log(array[i].value);
+            return array[i].value;
         // End If
         }
     // End look
     }
+}
+
+// Go through an array and count bombs (zero values)
+function resetBombCount(array) {
+    // Bomb count
+    let bombCount = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (getValue(array, i) === 0 ) {
+            bombCount++;
+        };
+    }
+    selectBombCount.textContent = bombCount;
 }
 
 // Convert Answers into HTML and display
@@ -77,45 +110,72 @@ function displayAnswer(arr) {
         // else if i + gridSize < arr.length
         if (i + gridSize >= arr.length) {
             // print hint to html
-            html += `<span id="id${arr[i].id}" class="hint">${arr[i].hint}</span>`;
+            html += `<span id="id${arr[i].id}" class="hint">88/88</span>`;
             // reset rowLength
             rowLength = 0;
         // if it's not the end of the row
         } else if (rowLength !== gridSize) {
             // if answer obj in array has id matching i
             if (arr[i].id === i) {
-                // print value to html
-                html += `<span id="id${arr[i].id}" class="card">${arr[i].value}</span>`;
+                // Create Card in HTML
+                html += `<span id="id${arr[i].id}" class="card">?</span>`;
                 // Count up rowLength
-                rowLength += 1;
+                rowLength++;
             // end if
             }
         // else
         } else { 
             // print hint to html
-            html += `<span id="id${arr[i].id}" class="hint">${arr[i].hint}</span><br>`;
+            html += `<span id="id${arr[i].id}" class="hint">88/88</span><br>`;
             // reset rowLength
             rowLength = 0;
         // end if
         }
     // end for
     }
-    selectLeftPlayspace.innerHTML = html;
+    selectPlayspace.innerHTML = html;
+}
+
+// All the runtime functions to retry a game
+function startGame() {
+    gameover = false;
+    resetScore();
+    populateAnswers();
+    resetBombCount(answers);
+    displayAnswer(answers);
 }
 
 /////////
 // Run //
 /////////
-populateAnswers();
-displayAnswer(answers);
+startGame();
 
 ///////////////
 // Listeners //
 ///////////////
 
-selectLeftPlayspace.addEventListener('click', (e) => {
-    const targ = e.target;
-    if (targ.className === 'card') {
-        getValue(answers, targ.id.split('id')[1]);
-    } 
+selectPlayspace.addEventListener('click', (e) => {
+    if (!gameover) {
+        const targ = e.target;
+        if (targ.className === 'card') {
+            const revealedValue = getValue(answers, targ.id.split('id')[1]);
+            targ.textContent = revealedValue;
+            // If revealed card is a bomb (zero) then color card red then end game
+            if (revealedValue === 0) {
+                targ.classList.add('revealed-bomb');
+                alert('Gameover!');
+                gameover = true;
+            // else if revealed card is not a bomb then award points
+            } else {
+                targ.classList.add('revealed-points');
+                points += revealedValue
+                selectScore.textContent = points;
+            }
+            
+        }
+    }
+});
+
+selectRestart.addEventListener('click', (e) => {
+    startGame();
 });
